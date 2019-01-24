94f39593de6929037d2f483c128d13d069633712
Julianna <julianna@berkeley.edu>
2019-01-24 00:04:50 -0800

    finished lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author julianna white
  */
 public class LeapYear {
 
@@ -12,6 +12,10 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+    /** checks if year is leapYear */
+    public static boolean isLeapYear(int year) {
+      return ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)));
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
@@ -30,4 +34,3 @@ public class LeapYear {
         }
     }
 }
-
