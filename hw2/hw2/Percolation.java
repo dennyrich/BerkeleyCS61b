09710d36cac7383b9df671eec26e90bb1a255c77
Denny Rich <dsrich@berkeley.edu>
2019-03-05 18:45:07 -0800

    removed unused import

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,6 +1,6 @@
 package hw2;
 
-import edu.princeton.cs.algs4.StdRandom;
+//import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
