615ccaba679d577df13053096bb3c34064abc3d6
Denny Rich <dsrich@berkeley.edu>
2019-03-17 16:25:46 -0700

    m

--- proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -125,6 +125,7 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
 
         try {
             PriorityNode<T> node = minHeap.get(index);
+            minHeap.get(500000000);
         } catch (IndexOutOfBoundsException e) {
 
             String heapString = "";
