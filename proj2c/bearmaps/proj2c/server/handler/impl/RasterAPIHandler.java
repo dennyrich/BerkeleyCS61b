96957d2e36d327229266ff7956c8ff4bbbcfc527
Denny Rich <dsrich@berkeley.edu>
2019-04-15 07:18:37 -0700

    depth = 0

--- proj2c/bearmaps/proj2c/server/handler/impl/RasterAPIHandler.java
+++ proj2c/bearmaps/proj2c/server/handler/impl/RasterAPIHandler.java
@@ -146,9 +146,9 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
         if (depth == 0) {
             results.put("raster_ul_lon", ROOT_ULLON);
             // goes to right side of xMax and yMax, hence the plus 1
-            results.put("raster_lr_lon", ROOT_ULLON);
+            results.put("raster_lr_lon", ROOT_LRLON);
             results.put("raster_ul_lat", ROOT_ULLAT);
-            results.put("raster_lr_lat", ROOT_ULLAT);
+            results.put("raster_lr_lat", ROOT_LRLAT);
 
         } else {
             results.put("raster_ul_lon", ROOT_ULLON + xMin * tileDeltaLon);
